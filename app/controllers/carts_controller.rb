0b0315f8cd591309982119714bc2c875347d5390
Honza Blazek <gimli@matfyz.cz>
2013-08-23 17:56:00 +0200

    next version of mail

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -125,7 +125,13 @@ class CartsController < ApplicationController
     end
     
     @message = @message + "\n\nNejsme plátci DPH.\nV případě jakýchkoli nesrovnalostí nás neváhej kontaktovat na emailu info@tumatemate.cz.\n\nDěkujeme za objednávku."
-    mail = ActionMailer::Base.mail(:from => "delivery@tumatemate.cz", :to => "info@tumatemate.cz", :subject => "[Maté]: objednávka #{params[:id]}", :message => @message)
+    @subject = "[Maté]: objednávka #{params[:id]}"
+    mail = Mail.new do
+      from "delivery@tumatemate.cz"
+      to "info@tumatemate.cz"
+      subject @subject
+      body @message
+    end
     mail.deliver
     session[:cart_id] = nil
   end
